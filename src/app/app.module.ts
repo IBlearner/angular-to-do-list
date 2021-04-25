fc85c3b5d8cfa54f81211b1a174db2d4ed248239
iblearner <kienvi96@hotmail.com>
2021-04-25 23:33:50 +1000

    Created new to-do list. Generated a task component to detail each task and a list component to store these tasks within.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
